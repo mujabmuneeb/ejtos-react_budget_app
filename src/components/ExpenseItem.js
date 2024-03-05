a42f45190da9776a40eaeec429da3847535435c9
Muhammad Abeer <mujabmuneeb@gmail.com>
2024-03-05 11:35:14 -0500

    practice lab

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,41 +1,45 @@
-import React, { useContext } from 'react'
-import { TiDelete } from 'react-icons/ti'
-import { AppContext } from '../context/AppContext'
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
+import { FcPlus } from "react-icons/fc";
+import { FaMinusCircle } from "react-icons/fa";
 
 const ExpenseItem = (props) => {
-  const { dispatch } = useContext(AppContext)
+    const { dispatch, currency } = useContext(AppContext);
 
-  const handleDeleteExpense = () => {
-    dispatch({
-      type: 'DELETE_EXPENSE',
-      payload: props.id,
-    })
-  }
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
 
-  const increaseAllocation = (name) => {
-    const expense = {
-      name: name,
-      cost: 10,
+    const increaseAllocation = (name, cost) => {
+        const expense = {
+            name: name,
+            cost: cost,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
     }
 
-    dispatch({
-      type: 'ADD_EXPENSE',
-      payload: expense,
-    })
-  }
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td><FcPlus size='1.5em' onClick={event => increaseAllocation(props.name, 10)}></FcPlus></td><td>
+                <FaMinusCircle
+                    size='1.5em'
+                    onClick={event => increaseAllocation(props.name, -10)}
+                    style={{ color: 'red' }}
+                />
+            </td>
+        </tr>
+    );
+};
 
-  return (
-    <tr>
-      <td>{props.name}</td>
-      <td>£{props.cost}</td>
-      <td>
-        <button onClick={(event) => increaseAllocation(props.name)}>+</button>
-      </td>
-      <td>
-        <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
-      </td>
-    </tr>
-  )
-}
-
-export default ExpenseItem
+export default ExpenseItem;
